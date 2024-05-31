b2ce7e7f23b802e24be14a965a001d9d5f391843
gerosignore <gsignore@etrr.edu.ar>
2024-05-31 14:11:43 -0300

    ejercicio 11 final

--- ejercicio 11.c
+++ ejercicio 11.c
@@ -2,13 +2,14 @@
 
 int main ()
 {
-    float medcm,medin,medpies;
+    float medcm,medin;
+    int medpies;
     printf("bienvenido, ingrese su medida en centimetros");
     scanf("%f",&medcm);
-    medin=medcm/2.54;
     medpies=medcm/30.48;
+    medin=medcm/2.54-medpies*12;
     printf("Su medida en pulgadas es: %f\n",medin);
-    printf("Su medida en pies es: %f",medpies);
+    printf("Su medida en pies es: %d",medpies);
     getchar();
     getchar();
 }
