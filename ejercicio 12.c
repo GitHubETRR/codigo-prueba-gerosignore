c52c4e8b803c7aaabced43cc2c67676617d51efa
gerosignore <gsignore@etrr.edu.ar>
2024-05-31 10:57:23 -0300

    Ejercicio 12 final

---
+++ ejercicio 12.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+
+    int main()
+    {
+    float EDAD,EDADdias;
+    printf("Bienvenido, ingrese cuantos anios vividos lleva:");
+    scanf("%f",&EDAD);
+    EDADdias=EDAD*365.25;
+    printf("Usted lleva %f",EDADdias);
+    printf(" dias vividos");
+    getchar();
+    getchar();
+    }
