6754e879f05296daa859393429585dfc67403231
gerosignore <gsignore@etrr.edu.ar>
2024-05-31 14:34:21 -0300

    ejercicio 16 final

--- ejercicio 11.c
+++ ejercicio 11.c
@@ -4,8 +4,8 @@ int main ()
 {
     float medcm,medin;
     int medpies;
-    printf("bienvenido, ingrese su medida en centimetros");
-    scanf("%f",&medcm);
+    printf("bienvenido, ingrese su medida en centimetros:");
+    scanf( "%f",&medcm);
     medpies=medcm/30.48;
     medin=medcm/2.54-medpies*12;
     printf("Su medida en pulgadas es: %f\n",medin);
--- ejercicio 16.c
+++ ejercicio 16.c
@@ -1,6 +1,13 @@
     #include <stdio.h>
+    #include <math.h>
 
     int main()
     {
-        
+    float superficie,perimetro;
+    printf("Bienvenido, ingrese la superficie del cuadrado:");
+    scanf("%f",&superficie);
+    perimetro=sqrt(superficie)*4;
+    printf("La medida del perimetro es de %f",perimetro);
+    getchar();
+    getchar();
     }
