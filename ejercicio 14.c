b6d084f82036853a4149d30c30c5f12afed7bf20
gerosignore <gsignore@etrr.edu.ar>
2024-05-31 11:57:31 -0300

    Ejercicio 14 final

---
+++ ejercicio 14.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+
+    int main()
+    {
+    float ang1,ang2,ang3;
+    printf("Bienvenido ingrese el valor del primer angulo del traingulo:");
+    scanf("%f",&ang1);
+    printf("ingrese el valor del segundo angulo del triangulo:");
+    scanf("%f",&ang2);
+    ang3=180-ang1-ang2;
+    printf("El valor del tercer angulo del triangulo %f grados",ang3);
+    getchar();
+    getchar();
+    }   
+
